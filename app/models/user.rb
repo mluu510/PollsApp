f8bbb9b91505df9bc16112d869e80ec7472bf6b2
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-18 19:33:24 -0700

    fixup User validations/associations some

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :user_name
+
+  validates :user_name, :presence => true
   
-  has_many :polls, :foreign_key => "author_id"
-  has_many :responses, :foreign_key => "respondent_id"
+  has_many(
+    :authored_polls,
+    :class_name => "Poll",
+    :foreign_key => :author_id,
+  )
+
+  has_many :responses, :foreign_key => :respondent_id
 end
