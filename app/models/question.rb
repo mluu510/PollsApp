9d5d9f5c684f01ffe7b9b7cb37c9605a26228041
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-18 23:17:29 -0700

    replace Poll#results with Question#results

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,29 @@ class Question < ActiveRecord::Base
 
   has_many :answer_choices
   belongs_to :poll
+
+  def results
+    select_sql = <<-SQL
+      answer_choices.*,
+      COUNT(responses.id) AS response_count
+    SQL
+
+    responses_joins_sql = <<-SQL
+      LEFT OUTER JOIN
+        responses ON answer_choices.id = responses.answer_choice_id
+    SQL
+
+    answer_choices = AnswerChoice
+      .select(select_sql)
+      .joins(:question)
+      .joins(responses_joins_sql)
+      .where("questions.poll_id = ?", self.id)
+      .group("answer_choices.id")
+
+    {}.tap do |results|
+      answer_choices.each do |answer_choice|
+        results[answer_choice.text] = Integer(answer_choice.response_count)x
+      end
+    end
+  end
 end
